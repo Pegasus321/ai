5f4437ce9a6c79182717caf75c2dbabf9e93e479
Pegasus321 <ayush.agrawal@vit.edu.in>
2023-10-23 23:47:37 +0530

    folder setup for sign-in/sign-up....and authentication with clerk

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,11 +1,9 @@
 import { Button } from "@/components/ui/button";
-import { Menu } from "lucide-react";
+
 export default function Navbar() {
   return (
     <div className="flex items-center p-4">
-      <Button>
-        <Menu />
-      </Button>
+      <Button></Button>
     </div>
   );
 }
