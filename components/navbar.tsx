d3e1834cca0d3b4ce6a9b4db7e090262dd16c5ea
Pegasus321 <ayush.agrawal@vit.edu.in>
2023-10-25 22:41:03 +0530

    folder setup for sign-in/sign-up....and authentication with clerk

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,9 +1,12 @@
 import { Button } from "@/components/ui/button";
+import { HamburgerMenuIcon } from "@radix-ui/react-icons";
 
 export default function Navbar() {
   return (
     <div className="flex items-center p-4">
-      <Button>click me</Button>
+      <Button>
+        <HamburgerMenuIcon />
+      </Button>
     </div>
   );
 }
