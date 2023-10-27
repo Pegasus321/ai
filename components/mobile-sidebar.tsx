d1561781117ccdd64c0f1dbb7c165a92e5c12b34
Pegasus321 <ayush.agrawal@vit.edu.in>
2023-10-27 23:15:51 +0530

    folder setup for sign-in/sign-up....and authentication with clerk

---
+++ components/mobile-sidebar.tsx
@@ -0,0 +1,31 @@
+"use client";
+
+import { HamburgerMenuIcon } from "@radix-ui/react-icons";
+
+import { Button } from "@/components/ui/button";
+import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
+import Sidebar from "./sidebar";
+import { useEffect, useState } from "react";
+
+export default function MobileSidebar() {
+  const [isMounted, setIsMounted] = useState(false);
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+  if (!isMounted) {
+    return null;
+  }
+  return (
+    <Sheet>
+      <SheetTrigger>
+        <Button variant="ghost" size="icon" className="md:hidden">
+          <HamburgerMenuIcon />
+        </Button>
+      </SheetTrigger>
+      <SheetContent side="left" className="p-0">
+        <Sidebar />
+      </SheetContent>
+    </Sheet>
+  );
+}
--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,12 +1,14 @@
-import { Button } from "@/components/ui/button";
-import { HamburgerMenuIcon } from "@radix-ui/react-icons";
+import { UserButton } from "@clerk/nextjs";
+
+import MobileSidebar from "./mobile-sidebar";
 
 export default function Navbar() {
   return (
     <div className="flex items-center p-4">
-      <Button>
-        <HamburgerMenuIcon />
-      </Button>
+      <MobileSidebar />
+      <div className="flex w-full justify-end">
+        <UserButton afterSignOutUrl="/" />
+      </div>
     </div>
   );
 }
